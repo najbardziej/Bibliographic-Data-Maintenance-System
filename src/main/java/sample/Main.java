959d55d757c01008a8307654d1298a19c4a9b0bb
pmasior <pmasior@outlook.com>
2020-06-06 12:58:38 +0200

    Add start GUI

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -8,9 +8,28 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main{
+public class Main extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("sample"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
 
     public static void main(String[] args) throws IOException {
+        launch();
         MyJavaObject my = new MyJavaObject();
         my.setTitle("Tytuł");
         my.setAuthor("Autor");
