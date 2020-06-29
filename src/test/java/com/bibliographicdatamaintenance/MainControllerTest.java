4b809bd4b40d956737928bf6cfaa3de440122e89
Remigiusz Najbar <muffespl@gmail.com>
2020-06-29 19:28:18 +0200

    Configured project - maven with junit

---
+++ src/test/java/com/bibliographicdatamaintenance/MainControllerTest.java
@@ -0,0 +1,12 @@
+package com.bibliographicdatamaintenance;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainControllerTest{
+    @Test
+    public void TestExample() {
+        assertEquals(1, 1);
+    }
+}
